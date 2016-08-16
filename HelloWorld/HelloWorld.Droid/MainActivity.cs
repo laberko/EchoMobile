f4ee92c9a3aafdaf3457922fa536678ef37f5617
laberko <mike@laberko.net>
2016-08-16 18:40:25 +0300

    60 Shades of Grey

--- HelloWorld/HelloWorld.Droid/MainActivity.cs
+++ HelloWorld/HelloWorld.Droid/MainActivity.cs
@@ -1,24 +1,19 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
-using HelloWorld;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace HelloWorld.Droid
 {
     [Activity(Label = "HelloWorld", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    public class MainActivity : FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            Xamarin.Forms.Forms.Init(this, bundle);
-            //LoadApplication(new App());
+            Forms.Init(this, bundle);
+            LoadApplication(new App());
         }
     }
 }
--- HelloWorld/HelloWorld/App.cs
+++ HelloWorld/HelloWorld/App.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace HelloWorld
 {
@@ -11,35 +6,19 @@ namespace HelloWorld
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
         {
-            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
         }
     }
 }
