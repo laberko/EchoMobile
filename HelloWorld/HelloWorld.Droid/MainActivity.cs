1b054f09c3a94bb67c0cd38fe909ead6a24bc5bd
laberko <mike@laberko.net>
2016-08-16 09:16:26 +0300

    PCL

---
+++ HelloWorld/HelloWorld.Droid/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using HelloWorld;
+
+namespace HelloWorld.Droid
+{
+    [Activity(Label = "HelloWorld", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            //LoadApplication(new App());
+        }
+    }
+}
+
